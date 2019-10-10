43ca9d9fb7a0cd362fa3e016ed8eafeacc6e9198
frojo <mojoz0101@gmail.com>
2019-10-10 14:15:13 -0400

    6 by 6 grid

--- vera/src/ofApp.cpp
+++ vera/src/ofApp.cpp
@@ -12,6 +12,22 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
+    ofSeedRandom(mouseX * 1000);
+    
+    for (int i = 0; i < 6; i++) {
+        for (int j = 0; j < 6; j++) {
+            
+            float x = ofMap(i, 0, 6, 50, 750);
+            float y = ofMap(j, 0, 6, 50, 750);
+            
+            ofBeginShape();
+            ofVertex(x, y);
+            ofVertex(x + 100, y);
+            ofVertex(x + 100, y + 100);
+            ofVertex(x, y + 100);
+            ofEndShape();
+        }
+    }
 
 }
 
