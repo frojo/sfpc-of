c7c893ab259e033ed12ee3665d570a4a08bdf501
frojo <mojoz0101@gmail.com>
2019-10-19 15:18:44 -0400

    got the basic path i think

--- whitney/src/ofApp.cpp
+++ whitney/src/ofApp.cpp
@@ -13,23 +13,28 @@ void ofApp::update(){
 //--------------------------------------------------------------
 void ofApp::draw(){
     float t = ofGetElapsedTimef();
-    float theta = t;
     float width = ofGetViewportWidth();
     float height = ofGetViewportHeight();
+    float theta = ofMap(mouseX, 0, height, 0, 2*PI);
+    
     
     float x_origin = width / 2;
-    float y_origin = height / 2;
-    float r = height / 2;
+    float y_origin = height;
+    
+    float sc = ofMap(mouseY, 0, width, 0, 2);
+    cout << sc << endl;
+    float r = height*sin(2*theta);
     
     
     // float x_circ = x_origin + r*cos(t);
-    float circleness = ofMap(ofClamp(t, 0, PI), 0, PI, 0, 1);
-    cout << circleness << endl;
-    float x =  x_origin + circleness * r*cos(theta + PI);
+    //float circleness = ofMap(ofClamp(t, 0, PI), 0, PI, 0, 1);
+    // cout << circleness << endl;
+   // float x =  x_origin * r*cos(theta);
+    float x = x_origin + .6*r*cos(theta);
     // float x =  x_origin;
     // float x = x_circ;
-    
-    float y = y_origin + r*sin(theta);
+    float y = y_origin + 1.2*r*sin(theta);
+    // float y = y_origin + r*sin(theta);
     
     ofDrawCircle(x, y, 10);
 
