e922b104cb2ec2522f5511f8b349c342a250ac95
frojo <mojoz0101@gmail.com>
2019-10-19 15:52:00 -0400

    add particle and start to try to get the timing/rate right

--- whitney/src/ofApp.cpp
+++ whitney/src/ofApp.cpp
@@ -15,28 +15,25 @@ void ofApp::draw(){
     float t = ofGetElapsedTimef();
     float width = ofGetViewportWidth();
     float height = ofGetViewportHeight();
-    float theta = ofMap(mouseX, 0, height, 0, 2*PI);
+    float theta_path = t/4 + PI / 4; // ofMap(mouseX, 0, width, 0, 2*PI);
     
     
     float x_origin = width / 2;
     float y_origin = height;
     
-    float sc = ofMap(mouseY, 0, width, 0, 2);
-    cout << sc << endl;
-    float r = height*sin(2*theta);
+    float r_path = height*sin(2*theta_path);
+    float x_path = x_origin + .6*r_path*cos(theta_path);
+    float y_path = y_origin + 1.2*r_path*sin(theta_path);
     
     
-    // float x_circ = x_origin + r*cos(t);
-    //float circleness = ofMap(ofClamp(t, 0, PI), 0, PI, 0, 1);
-    // cout << circleness << endl;
-   // float x =  x_origin * r*cos(theta);
-    float x = x_origin + .6*r*cos(theta);
-    // float x =  x_origin;
-    // float x = x_circ;
-    float y = y_origin + 1.2*r*sin(theta);
-    // float y = y_origin + r*sin(theta);
+    // particles
+    float p_x = x_path + 100*sin(t*10);
+    float p_y = y_path;
     
-    ofDrawCircle(x, y, 10);
+    ofSetColor(0);
+    ofDrawCircle(p_x, p_y, 5);
+    ofSetColor(255);
+    ofDrawCircle(x_path, y_path, 10);
 
 }
 
