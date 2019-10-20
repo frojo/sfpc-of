9c966010ee85be65cf3aba0db6b66796cf29d717
frojo <mojoz0101@gmail.com>
2019-10-20 18:11:46 -0400

    got a path of particles

--- whitney/src/ofApp.cpp
+++ whitney/src/ofApp.cpp
@@ -12,7 +12,8 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-    float t = ofGetElapsedTimef();
+    float limit = 8;
+    float t = ofWrap(ofGetElapsedTimef(), 0, 12);
     float width = ofGetViewportWidth();
     float height = ofGetViewportHeight();
     float theta_path = t/4 + PI / 4; // ofMap(mouseX, 0, width, 0, 2*PI);
@@ -25,15 +26,35 @@ void ofApp::draw(){
     float x_path = x_origin + .6*r_path*cos(theta_path);
     float y_path = y_origin + 1.2*r_path*sin(theta_path);
     
-    
-    // particles
-    float p_x = x_path + 100*sin(t*10);
-    float p_y = y_path;
-    
+    // draw the bees
     ofSetColor(0);
-    ofDrawCircle(p_x, p_y, 5);
+    for (int i = 0; i < 100; i++) {
+        
+        cout << mouseX << endl;
+        float theta_particle = theta_path - ((float)i)/mouseX;
+        float r_particle = height*sin(2*theta_particle);
+        float x_particle_center = x_origin + .6*r_particle*cos(theta_particle);
+        float y_particle_center = y_origin + 1.2*r_particle*sin(theta_particle);
+        
+        float x_particle = x_particle_center; //  + 100*sin(t*10);
+        float y_particle = y_particle_center;
+        
+        ofDrawCircle(x_particle_center, y_particle_center, 5);
+        
+    }
+    // ofDrawCircle(p_x, p_y, 5);
+    
+    
     ofSetColor(255);
     ofDrawCircle(x_path, y_path, 10);
+    
+    float theta_path_2 = theta_path - .1;
+    float r_path_2 = height*sin(2*theta_path_2);
+    float x_path_2 = x_origin + .6*r_path_2*cos(theta_path_2);
+    float y_path_2 = y_origin + 1.2*r_path_2*sin(theta_path_2);
+    
+    ofSetColor(100);
+    ofDrawCircle(x_path_2, y_path_2, 10);
 
 }
 
